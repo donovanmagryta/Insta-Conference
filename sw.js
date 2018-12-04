51efeebffc9e22515ef5af1625033d3afb0651cd
Donovan <triops124@gmail.com>
2018-12-04 10:11:49 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
+var APP_PREFIX = 'InstaConference_'     // Identifier for this app (this needs to be consistent across every cache update)
 var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            // Add URL you want to cache in this list.
-  '/{repository}/',                     // If you have separate JS/CSS files,
-  '/{repository}/index.html'            // add path to those files here
+  '/insta-conference/',                     // If you have separate JS/CSS files,
+  '/insta-conference/index.html'            // add path to those files here
 ]
 
 // Respond with cached resources
